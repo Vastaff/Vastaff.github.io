e9f25a901010d2c4059a76e05cd95c17afb57587
Vastaff <va2578735@gmail.com>
2024-06-29 18:08:46 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-/* import { TonConnectUIProvider } from '@tonconnect/ui-react'; */
-import WebApp from '@twa-dev/sdk'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'; */
 
-WebApp.ready();
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
-/* const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
- */
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  /* <TonConnectUIProvider manifestUrl={manifestUrl}> */
-   <React.StrictMode>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,  
- /*  </TonConnectUIProvider>, */
+  </TonConnectUIProvider>, 
 )
